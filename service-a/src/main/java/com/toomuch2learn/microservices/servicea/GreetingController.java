021778c8c9e662a8a1e1c6c786ea7b6dc7668d2e
Carlos L. krlosaren@gmail.com <krlosaren@gmail.com>
2023-09-08 23:11:02 -0300

    [ADD] add issue

--- service-a/src/main/java/com/toomuch2learn/microservices/servicea/GreetingController.java
+++ service-a/src/main/java/com/toomuch2learn/microservices/servicea/GreetingController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.h2.security.SHA256;
 
 
 @RestController
